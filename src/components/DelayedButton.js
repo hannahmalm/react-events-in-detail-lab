8d8246033ecf2e101010694dd0f25494a427a7fa
Hannah Malm <hannahemalm616@gmail.com>
2021-09-11 22:10:57 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,25 @@
 // Code DelayedButton Component Here
+//In the components/DelayedButton.js file, create a DelayedButton React component
+//This component takes two props: onDelayedClick (a function), and delay (a number).
+//Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+//If successful, the event will be logged to the console once the timeout has finished.
+//remember event pooling in React. By the time the setTimeout fires, the event object will have already been returned to the pool.
+
+import React from 'react';
+export default class DelayedButton extends React.Component {
+    //Event pooling means that whenever an event fires, its event data (an object) is sent to the callback.
+    handleClick = (event) => {
+
+        //As of v17, e.persist() doesn’t do anything because the SyntheticEvent is no longer pooled.
+        //need to access your event data in an asynchronous manner
+        //we can make the event persistent by calling that method: event.persist().
+        event.persist();
+        window.setTimeout(() => {
+            this.props.onDelayedClick(event);
+        }, this.props.delay);
+    };
+  
+    render() {
+      return <button onClick={this.handleClick}>Delayed</button>;
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import CoordinatesButton from './components/CoordinatesButton';
 import DelayedButton from './components/DelayedButton';
 
+//code for a basic React app
+//Two of the components referenced, CoordinatesButton and DelayedButton, are being passed props.
 ReactDOM.render(
   <div>
     <CoordinatesButton onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates) } />
@@ -12,3 +14,11 @@ ReactDOM.render(
   </div>,
   document.getElementById('global')
 );
+
+
+//event.clientX and event.clientY. These provide the current x/y position of a mouse
+//The clientX read-only property of the MouseEvent interface provides the horizontal coordinate within the application's viewport at which the event occurred
+  //var x = instanceOfMouseEvent.clientX
+//The clientY read-only property of the MouseEvent interface provides the vertical coordinate within the application's viewport at which the event occurred 
+  //For example, clicking on the top edge of the viewport will always result in a mouse event with a clientY value of 0, regardless of whether the page is scrolled vertically.
+  //var y = instanceOfMouseEvent.clientY
